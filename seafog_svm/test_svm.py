19256420f2fb0cdb96b8ff2b5510a08c323312cf
SillyPasty <47266819+SillyPasty@users.noreply.github.com>
2020-08-19 20:08:27 +0800

    服务器训练

--- seafog_svm/test_svm.py
+++ seafog_svm/test_svm.py
@@ -1,26 +1,22 @@
 # used on the server
-# Generate datasets from vfm datasets
-# Him + Calipso
-
-import csv
+# 
+import time
 import os
 import datetime
 import cv2
 from PIL import Image
 import numpy as np
-import time
-import math
 import joblib
-import pysolar
-import pytz
+from pysolar import solar
+from pytz import timezone
 
 SPACE_R = 0.0125
 START_LAT = 45.
 START_LON = 105.
 
-def get_axis(lat, lon):
-    x_axis = int((START_LAT - float(lat)) / SPACE_R)
-    y_axis = int((float(lon) - START_LON) / SPACE_R)
+def get_axis(lat, lon, space_r):
+    x_axis = int((START_LAT - float(lat)) / space_r)
+    y_axis = int((float(lon) - START_LON) / space_r)
     
     if x_axis == 2160: x_axis -= 1
     if y_axis == 3600: y_axis -= 1
@@ -60,7 +56,7 @@ def get_him_data(path_dic, him_time):
     for i in range(1, 17):
         channel_str = 'B{:0>2d}'.format(i)
 
-        him_file_dir = get_file_dir(path_dic, him_time)
+        him_file_dir = path_dic
 
         if him_file_dir == None:
             him_data.append(None)
@@ -83,7 +79,7 @@ def get_him_data(path_dic, him_time):
     return him_data
 
 def plot_mask(origin_map_path, mask, target_area, save_path):
-    x_axis1, y_axis1 = get_axis(target_area['lat1'], target_area['lon1'])
+    x_axis1, y_axis1 = get_axis(target_area['lat1'], target_area['lon1'], SPACE_R)
     x, y = mask.shape[0], mask.shape[1]
     real_img = cv2.imread(origin_map_path)
     for i in range(x):
@@ -97,64 +93,66 @@ def plot_mask(origin_map_path, mask, target_area, save_path):
     cv2.imwrite(save_path + '.png', real_img)
 
 
-def test(model_path, data_path, datetime, target_area):
+def test(model, data_path, datetime, target_area):
+    print('Reading data...')
     him_data = get_him_data(data_path, datetime)
     solar_angle = get_solar_angle(datetime, him_data.shape)
     test_data = np.concatenate((him_data, solar_angle), axis=2)
     assert test_data.shape == (2160, 3600, 17), print(test_data.shape)
 
-    x_axis1, y_axis1 = get_axis(target_area['lat1'], target_area['lon1'])
-    x_axis2, y_axis2 = get_axis(target_area['lat2'], target_area['lon2'])
+    x_axis1, y_axis1 = get_axis(target_area['lat1'], target_area['lon1'], SPACE_R)
+    x_axis2, y_axis2 = get_axis(target_area['lat2'], target_area['lon2'], SPACE_R)
     test_data = test_data[x_axis1:x_axis2, y_axis1: y_axis2, :]
     origin_shape = (test_data.shape[0], test_data.shape[1])
     test_data = test_data.reshape(-1, 17)
-    
-    model = joblib.load(model_path + '.pkl')
+    print('Predicting...')
     predicted = model.predic(test_data)
 
     predict_mask = predicted.reshape(origin_shape[0], origin_shape[1], -1)
     return predict_mask
 
-    
-    
-
 def main():
     start_time = time.time()
     path_dic = get_path_dic()
-    model_path = ''
+    model_path = os.path.join('calipso', 'model', 'svm_')
     output_path = os.path.join('calipso', 'predicted')
-    origin_map_path = ''
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    origin_map_path = os.path.join('calipso', 'map.png')
     target_area = {
-        'lat1':1,
-        'lon1':2,
-        'lat1':3,
-        'lat2':4
+        'lat1':41.,
+        'lon1':117.5,
+        'lat1':34,
+        'lat2':127
     }
     time_range = {
-        'year': [2019],
-        'month': [i for i in range(1, 13)],
-        'day': [i for i in range(1, 32)],
+        'year': [2020],
+        'month': [5],
+        'day': [1],
         'hour': [i for i in range(24)],
-        'minutes': [0, 30]
+        'minutes': [0]
     }
+    model_day = joblib.load(model_path + 'day.pkl')
+    model_night = joblib.load(model_path + 'night.pkl')
     for year in time_range['year']:
         for month in time_range['month']:
             for day in time_range['day']:
                 for hour in time_range['hour']:
+                    model = model_day if hour < 12 else model_night
                     for minute in time_range['minutes']:
                         dt = None
                         try:
-                            dt = datetime.datetime(year=year, month=month, day=day,hour=hour, minute=minute)
+                            dt = datetime.datetime(year=year, month=month, day=day,hour=hour, minute=minute, tzinfo=timezone('UTC'))
                         except Exception as e:
                             continue
+                        print('Predicting date:' + str(dt))
                         file_dir = get_file_dir(path_dic, dt)
                         if file_dir == None:
                             continue
-                        mask = test(model_path, file_dir, dt, target_area)
-                        
+                        mask = test(model, file_dir, dt, target_area)
+                        print('Plotting result...')
                         result_path = os.path.join(output_path, dt.strftime('%Y-%m-%d %H:%M'))
-                        plot_mask(origin_map_path, mask, target_area,  )
-
+                        plot_mask(origin_map_path, mask, target_area, result_path)
 
     print('Time cost: ', str(time.time() - start_time))
 

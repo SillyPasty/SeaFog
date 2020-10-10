bfc5d470328d66160b3aff53470994e4ab11de7e
SillyPasty <1179041704@qq.com>
2020-10-10 15:12:01 +0800

    优化图像采样算法

--- OUCseafog/gen_dataset.py
+++ OUCseafog/gen_dataset.py
@@ -6,6 +6,22 @@ from util import him
 
 
 
+
+
+sample_mask1 = np.zeros((1041, 651), dtype=np.int)
+sample_mask2 = np.zeros((851, 1361), dtype=np.int)
+idx = 0
+for i in range(1042):
+    if i % 8 == 0 or i % 8 == 2 or i % 8 == 3 or i % 8 == 5 or i % 8 == 6:
+        sample_mask1[i, idx] = 1
+        idx += 1
+idx = 0
+for i in range(1362):
+    if i % 8 == 0 or i % 8 == 2 or i % 8 == 3 or i % 8 == 5 or i % 8 == 6:
+        sample_mask2[idx, i] = 1
+        idx += 1
+
+
 def get_dt(fn):
     dt = datetime.strptime(fn[:-4], "%Y%m%d%H%M")
     return dt
@@ -19,18 +35,7 @@ def sample(img):
     """ 0 0.0125  0.025  0.0375  0.05  0.0625  0.075  0.0875  0.1
         0         0.02   0.04          0.06    0.08           0.1
     """
-    sampled_img = np.zeros((cfg.OUC.SIZE[0], cfg.OUC.SIZE[1]), dtype=img.dtype)
-    tmp_img = np.zeros((cfg.OUC.SIZE[0], img.shape[1]), dtype=img.dtype)
-    tmp_idx = 0
-    for i in range(img.shape[0]):
-        if i % 8 == 0 or i % 8 == 2 or i % 8 == 3 or i % 8 == 5 or i % 8 == 6:
-            tmp_img[tmp_idx, :] = img[i, :].copy()
-            tmp_idx += 1
-    tmp_idx = 0
-    for i in range(img.shape[1]):
-        if i % 8 == 0 or i % 8 == 2 or i % 8 == 3 or i % 8 == 5 or i % 8 == 6:
-            sampled_img[:, tmp_idx] = tmp_img[:, i].copy()
-            tmp_idx += 1
+    sampled_img = np.dot(sample_mask2, np.dot(img, sample_mask1))
     return sampled_img
 
 def get_simple(ouc_fn, path_dic):
@@ -41,7 +46,6 @@ def get_simple(ouc_fn, path_dic):
     for i in range(1, 17):
         ch_img, flag = him.get_channel_img(dt, fd, i)
         if flag == False:
-            inv = 1
             continue
         ch_img = him.get_target_area(cfg.OUC.START_LON, cfg.OUC.END_LON, cfg.OUC.START_LAT, cfg.OUC.END_LAT, ch_img)
         ch_img = sample(ch_img)
@@ -84,13 +88,14 @@ def main(ouc_dir):
         print('\rProcessing {}'.format(ouc_fn), end='')
         result = stat_data(ouc_fn)
         res = merge_dic(res, result)
+        break
     print()
     print(res)
 
 path_dic = him.get_path_dic(r'数据\himexp')
 ouc_fn = r'数据\海大反演\参考夜间结果\2019\201903140000.dat'
-# get_simple(ouc_fn, path_dic)
-main(r'数据\海大反演\参考夜间结果\2020')
+get_simple(ouc_fn, path_dic)
+# main(r'数据\海大反演\参考夜间结果\2020')
 
 # 0.0: 405584026, 1.0: 1288106302, 2.0: 670944089, 3.0: 16461881
 # 0.0: 148165206, 1.0: 621138112, 2.0: 257678007, 3.0: 16143758
